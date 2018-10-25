476461cb9beb0d2286091c4b60f44121927b0aff
hamer073 <hamer073@morris.umn.edu>
2018-10-25 11:36:15 -0500

    Add logic for ExchangeRateReader - Add logic for just getting the exchange rate from the base for a given Country Code on a given Date - Add logic for getting the conversion between two countries given a Country Code and a Date - Add a helper function generateURL to generate the proper form of the URL

--- src/xrate/ExchangeRateReader.java
+++ src/xrate/ExchangeRateReader.java
@@ -1,13 +1,20 @@
 package xrate;
 
-import java.io.IOException;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Provide access to basic currency exchange rate services.
  * 
- * @author PUT YOUR TEAM NAME HERE
+ * @author team-pizza: Ethan Hamer and
  */
 public class ExchangeRateReader {
+    String baseUrl;
+    String accessKey;
 
     /**
      * Construct an exchange rate reader using the given base URL. All requests
@@ -17,10 +24,20 @@ public class ExchangeRateReader {
      * year, month, and day; the URL for 25 June 2010, for example, would be
      * http://api.finance.xaviermedia.com/api/2010/06/25.xml
      * 
-     * @param baseURL
+     * @param newURL
      *            the base URL for requests
      */
-    public ExchangeRateReader(String baseURL) {
+    public ExchangeRateReader(String newURL) throws FileNotFoundException, IOException {
+            this.baseUrl = newURL;
+        try {
+            this.accessKey = new BufferedReader(new FileReader("src/accessKey")).readLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("Make sure the file exists");
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+            System.out.println(e);
+        }
         // TODO Your code here
         /*
          * DON'T DO MUCH HERE!
@@ -47,7 +64,44 @@ public class ExchangeRateReader {
      */
     public float getExchangeRate(String currencyCode, int year, int month, int day) throws IOException {
         // TODO Your code here
-        throw new UnsupportedOperationException();
+        URL completeURL = generateURL(year, month, day);
+
+        InputStream inputStream = completeURL.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        JsonParser parser = new JsonParser();
+        JsonObject rawData = parser.parse(reader).getAsJsonObject();
+
+        JsonElement exchangeRate = rawData.getAsJsonObject("rates").get(currencyCode);
+        Float parsedExchangeRate = exchangeRate.getAsFloat();
+
+        return parsedExchangeRate;
+    }
+
+    private URL generateURL(int year, int month, int day) throws MalformedURLException {
+        String yearString = String.valueOf(year);
+        String monthString;
+        String dayString;
+        if(month < 10){
+            monthString = "0" + String.valueOf(month);
+        } else {
+            monthString = String.valueOf(month);
+        }
+        if(day < 10){
+            dayString = "0" + String.valueOf(day);
+        } else {
+            dayString = String.valueOf(day);
+        }
+
+        String completeURLString = baseUrl + yearString + "-" + monthString + "-" + dayString + "?access_key=" + this.accessKey;
+        URL completeURL = null;
+        try {
+            completeURL =  new URL(completeURLString);
+        } catch (MalformedURLException e) {
+            System.out.println("Make sure you have a proper URL");
+            System.out.println(e);
+        }
+        return completeURL;
     }
 
     /**
@@ -71,6 +125,17 @@ public class ExchangeRateReader {
             String fromCurrency, String toCurrency,
             int year, int month, int day) throws IOException {
         // TODO Your code here
-        throw new UnsupportedOperationException();
+        URL completeURL = generateURL(year, month, day);
+
+        InputStream inputStream = completeURL.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        JsonParser parser = new JsonParser();
+        JsonObject rawData = parser.parse(reader).getAsJsonObject();
+
+        Float fromCurrencyValue = rawData.getAsJsonObject("rates").get(fromCurrency).getAsFloat();
+        Float toCurrencyValue = rawData.getAsJsonObject("rates").get(toCurrency).getAsFloat();
+
+        return fromCurrencyValue/toCurrencyValue;
     }
 }
